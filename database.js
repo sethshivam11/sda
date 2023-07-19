f84aa7389ac7d06cd7af30081f1f8f355e77f741
blaisepascal287 <130232913+blaisepascal287@users.noreply.github.com>
2023-07-19 17:24:26 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const MONGODB_URL = process.env;
 
 exports.connect = () => {
+  mongoose.set("strictQuery", false);
   mongoose.connect(MONGODB_URL , {
     useNewUrlParser: true,
     useUnifiedTopology: true
